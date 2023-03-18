9f7f73b3de3cef95efb13102e18a12aed4b0ca73
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-18 14:57:32 +0300

    Average of elements in array and map with closures added

---
+++ SwiftLeetCode.playground/Pages/Average of elements in array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+// Среднее арифметическое элементов массива
+
+func averageOfElements(in numbers: [Int]) -> Double {
+    let sum = Double(numbers.reduce(0, +))
+    let average = sum / Double(numbers.count)
+    
+    return average
+}
+
+averageOfElements(in: [4, 7, 12, 5])
+
+// Использование функции map и замыканий
+
+var numbers = [1, 15, 10]
+
+let sum = 0
+
+numbers = numbers.map { $0 < 2 ? 5 : 2 }
+print(numbers)
+
