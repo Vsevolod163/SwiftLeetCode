a7d2b26ba44f79c555c468f21db0a2cda8c3bfcd
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-25 21:02:05 +0300

    Second variant with map function added

--- SwiftLeetCode.playground/Pages/Sum of positive and  negative.xcplaygroundpage/Contents.swift
+++ SwiftLeetCode.playground/Pages/Sum of positive and  negative.xcplaygroundpage/Contents.swift
@@ -27,3 +27,20 @@ let numbers = createRandomArray(countOfElements: 12)
 
 findSumOfPositiveElementsIn(numbers: numbers)
 findSumOfNegativeElementsIn(numbers: numbers)
+
+// Второй вариант (использование map)
+
+// Создаем массив из 12 случайных чисел в диапазоне [-9, 9]
+let numbersV2 = (1...12).map { _ in Int.random(in: -9...9) }
+print("Массив: \(numbersV2)")
+
+// Находим сумму положительных и отрицательных элементов массива с помощью reduce
+let (positiveSum, negativeSum) = numbersV2.reduce((0, 0)) { (result, number) in
+    if number >= 0 {
+        return (result.0 + number, result.1)
+    } else {
+        return (result.0, result.1 + number)
+    }
+}
+print("Сумма положительных элементов: \(positiveSum)")
+print("Сумма отрицательных элементов: \(negativeSum)")
