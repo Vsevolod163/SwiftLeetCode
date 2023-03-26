792188f5fc5c7b8e37c1c1eaac7882a1679b8ef4
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-26 23:42:59 +0300

    start of binary search

---
+++ SwiftLeetCode.playground/Pages/Binary search.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+
+func makeBinarySearchIn(numbers: [Int], for number: Int) -> [Int] {
+    var finish = false
+    var startIndex = 0
+    
+    while !finish {
+        for index in numbers.indices where index >= startIndex {
+            
+        }
+    }
+    
+    return 0
+}
