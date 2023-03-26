71c343d99774f38deca8ed86e7113e68076aa60f
Vsevolod <vsevolod.lashin@gmail.com>
2023-03-27 01:37:26 +0300

    Ex binary search added

--- SwiftLeetCode.playground/Pages/Binary search.xcplaygroundpage/Contents.swift
+++ SwiftLeetCode.playground/Pages/Binary search.xcplaygroundpage/Contents.swift
@@ -1,13 +1,23 @@
 
-func makeBinarySearchIn(numbers: [Int], for number: Int) -> [Int] {
-    var finish = false
+func makeBinarySearchIn(numbers: [Int], for number: Int) -> Int {
     var startIndex = 0
-    
-    while !finish {
-        for index in numbers.indices where index >= startIndex {
-            
+    var endIndex = numbers.count - 1
+ 
+    while startIndex <= endIndex {
+        let middleIndex = (startIndex + endIndex) / 2
+        
+        if numbers[middleIndex] == number {
+            return middleIndex
+        } else if numbers[middleIndex] < number {
+            startIndex = middleIndex + 1
+        } else {
+            endIndex = middleIndex - 1
         }
     }
     
-    return 0
+    return -1
 }
+
+makeBinarySearchIn(numbers: [-100, -11, -2, 4, 8, 19, 100, 200, 300], for: -11)
+
+
